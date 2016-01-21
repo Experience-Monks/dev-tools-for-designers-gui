0524ba1479e60d5c3603a1ac4c87b8d322052da5
nascherman <n_scherman@hotmail.com>
2016-01-20 20:32:14 -0800

    update xcrun

--- main.js
+++ main.js
@@ -9,9 +9,8 @@ let mainWindow;
 function createWindow() {
 	mainWindow = new BrowserWindow({width: 800, height: 600});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
-		//for debugging
-	//mainWindow.webContents.openDevTools();
-	
+	//Dev tools
+	mainWindow.webContents.openDevTools();	
 		// Quit when all windows are closed.
 	mainWindow.on('closed', function() {
 		mainWindow = null;
